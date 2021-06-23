e7069735ede6499d8d2b5dd2ef2f2932827d0791
Ophelia Heard <opheliaheard@Ophelias-MBP.attlocal.net>
2021-06-23 00:22:50 -0500

    dept of each employee (4)

--- PH_queries.sql
+++ PH_queries.sql
@@ -19,10 +19,29 @@ where DATE_PART('year',hire_date) = 1986
 order by emp_no;
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+select dept_manager.dept_no, 
+departments.dept_name,
+dept_manager.emp_no,
+employees.last_name,
+employees.first_name
+from departments
+join dept_manager
+on departments.dept_no = dept_manager.dept_no
+left join employees 
+on dept_manager.emp_no = employees.emp_no
+order by emp_no
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+select employees.emp_no, 
+employees.last_name,
+employees.first_name,
+dept_emp.dept_no,
+departments.dept_name
+from employees
+join dept_emp
+on dept_emp.emp_no = employees.emp_no
+join departments
+on dept_emp.dept_no = departments.dept_no 
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
