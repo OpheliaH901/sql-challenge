014bbe846fa54629c00cc71f131898bea0e3b47f
Ophelia Heard <opheliaheard@Ophelias-MBP.attlocal.net>
2021-06-22 23:45:39 -0500

    1986 employees

--- PH_queries.sql
+++ PH_queries.sql
@@ -1,16 +1,22 @@
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT employees.emp_no, 
+select employees.emp_no, 
 employees.last_name,
 employees.first_name,
 employees.sex,
 salaries.salary
-FROM employees
-LEFT JOIN salaries
-ON employees.emp_no = salaries.emp_no
-ORDER BY emp_no
+from employees
+Left join salaries
+on employees.emp_no = salaries.emp_no
+order by emp_no
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-
+select employees.emp_no, 
+employees.last_name,
+employees.first_name,
+employees.hire_date
+from employees
+where DATE_PART('year',hire_date) = 1986
+order by emp_no;
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
